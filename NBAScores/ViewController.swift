656be21b2da3696144b0a246a24f15b99dd31217
Anthony Pham <akpham95@gmail.com>
2022-12-19 15:15:39 -0600

    Initial Commit

---
+++ NBAScores/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NBAScores
+//
+//  Created by Anthony Pham on 12/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
